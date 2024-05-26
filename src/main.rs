926cb86f99373d7ebbacf5d3c9a12a22b116a57c
safizn <nassar2@wisc.edu>
2024-05-25 19:55:04 -0500

    basic file server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use actix_files::Files;
-use actix_web::web::ServiceConfig;
+use actix_web::{get, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+
+#[get("/")]
+async fn hello_world() -> &'static str {
+    "Hello World!"
+}
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
+        // cfg.service(hello_world);
         cfg.service(Files::new("/", "assets"));
     };
 
